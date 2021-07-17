f609d26853b2536648951276d522431a25808c79
Ashis Paul <ashis.paul@compass.com>
2021-07-17 22:38:13 +0530

    goroutine addded

--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-    "github.com/deckarep/gosx-notifier"
 	"fmt"
 	"time"
 )
-
-func notify(text string) {
-	note := gosxnotifier.NewNotification("Join the google meet")
-    note.Title = "Time for Meeting"
-    note.Subtitle = text
-    note.Sound = gosxnotifier.Default
-	note.Sender = "com.apple.Safari"
-	note.Push()	
-}
-
-
 
 func main() {
 	fetchEvents()
